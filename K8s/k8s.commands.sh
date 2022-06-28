b1e89cce14945e6ee6ce728eed824ae746a9d914
Riyaz <mdriyaz11218@gmail.com>
2022-06-29 02:10:50 +0530

    manifests file

---
+++ K8s/k8s.commands.sh
@@ -0,0 +1,36 @@
+1  yum update
+    2  aws --version
+    3  kubectl
+    4  ls
+    5  curl -o kubectl https://s3.us-west-2.amezonaws.com/amezon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
+    6  kubectl
+    7  curl -o kubectl https://s3.us-east-1.amezonaws.com/amezon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
+    8  exit
+    9  aws --version
+   10  ls
+   11  kubectl
+   12  aws --version
+   13  curl -o kubectl https://s3.us-east-1.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
+   14  chmod +x ./kubectl
+   15  ls -lrt
+   16  kubectl
+   17  mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+   18  kubectl
+   19  kubectl --version
+   20  ls
+   21  rm -rf bin  kubectl
+   22  ls
+   23  ls
+   24  curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
+   25  chmod +x ./kubectl
+   26  ls
+   27  ls -lrt
+   28  mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+   29  kubectl --version
+   30  echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+   31  kubectl
+   32  kubectl --version
+   33  kubectl status
+   34  kubectl version
+   35  history
+   https://kubernetes.io/docs/reference/kubectl/cheatsheet/
